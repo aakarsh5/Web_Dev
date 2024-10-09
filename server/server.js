7d19be8637f504e0153f44b94230aef01ad739d2
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2024-10-09 19:28:10 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome ");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server is running in port ${PORT}`);
+});
